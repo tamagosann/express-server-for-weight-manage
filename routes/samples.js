e5c0b2e3d3dc09406f863505746558dda1fac251
tamagosann <hssvs619@gmail.com>
2021-06-16 20:40:04 +0900

    modified cors

--- routes/samples.js
+++ routes/samples.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 router.use(
   cors({
-    origin: "http://localhost:3000", //アクセス許可するオリジン
+    origin: "http://localhost:3001", //アクセス許可するオリジン
     credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
     optionsSuccessStatus: 200, //レスポンスstatusを200に設定
   })
